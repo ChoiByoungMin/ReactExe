de56b0483f39c2565a0d806dfc7cc5bff74e72b1
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-05-20 15:00:07 +0900

    event, ref 사용

--- React_Exe/ch06_event/src/App.js
+++ React_Exe/ch06_event/src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 // import EventPratice from "./ex03_onChange_state/EventPractice";
 // import EventPratice from "./ex04_onChange_state/EventPractice";
 // import EventPratice from "./ex05_function_bind/EventPractice";
-import EventPratice from "./ex06_arrow/EventPractice";
+// import EventPratice from "./ex06_arrow/EventPractice";
+// import EventPratice from "./ex07_multi_setstate/EventPractice";
+// import EventPratice from "./ex08_onKeyPress/EventPractice";
+// import EventPratice from "./ex09_function_event/EventPractice";
+import EventPratice from "./ex10_function_multi_setstate/EventPractice";
 
 function App() {
   return <EventPratice />;
