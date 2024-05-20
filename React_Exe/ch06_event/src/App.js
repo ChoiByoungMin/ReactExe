297e69d3f60d155e1de0cb349661b206298ba6d9
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-05-20 11:52:22 +0900

    이벤트 처리

---
+++ React_Exe/ch06_event/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+// import EventPratice from "./ex02_onChange/EventPractice";
+// import EventPratice from "./ex03_onChange_state/EventPractice";
+// import EventPratice from "./ex04_onChange_state/EventPractice";
+// import EventPratice from "./ex05_function_bind/EventPractice";
+import EventPratice from "./ex06_arrow/EventPractice";
+
+function App() {
+  return <EventPratice />;
+}
+
+export default App;
---
+++ React_Exe/ch06_event/src/ex01_html_react_event/Say.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const Say = () =>{
+    /* [변수명, setter함수] = useState(초기값);
+
+        setter 함수르 통해서 변수값을 변화시켜야
+        Reactdml Hook이 작동되어서
+        화면에 변수가 사용된다면 변화를 감지해서  Render가 이루어진다.
+    */
+    const [message, setMessage] = useState('');
+    const onClickEnter = () => setMessage('안녕하세요');
+    const onClickLeave = () => setMessage('안녕히 가세요');
+
+    return(
+        <div>
+            {/* html에서의 onclick이 아니라 Camel Case로 이벤트를 표시 */}
+            <button onClick={onClickEnter}>입장</button>
+            <button onClick={onClickLeave}>퇴장</button>
+            <h1>{message}</h1>
+        </div>
+    )
+}
+
+export default Say;
