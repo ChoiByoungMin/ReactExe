e3bff81e761f5f4cf71251d426b6f9582b0474ac
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-05-20 18:00:37 +0900

    lifecycle

---
+++ React_Exe/ch08_array_component/src/ex05_remove_name/iterationSample.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+ 
+const IterationSample = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' }
+  ]);
+  const [inputText, setInputText] = useState('');
+
+  // 현재 초기값의 마지막 id가 4이므로,다음 id는 5를 제공해야 하므로
+  const [nextId, setNextId] = useState(5);
+  
+  const onChange = e => setInputText(e.target.value);
+  const onClick = () => {
+    // 새로 부여된 id와 입력데이터를 가져와서, names에 새로 추가
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    })
+    setNextId(nextId +1);
+    setNames(nextNames);
+    setInputText("");
+  }
+  const onRemove = id => {
+    const nextNames = names.filter(name => name.id !== id);
+    setNames(nextNames);
+  };
+  const namesList = names.map(name => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>
+    </>
+  );
+};
+ 
+export default IterationSample;
---
+++ React_Exe/ch09_lifecycle/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NotificationList from "./components/NotifictionList";
+
+function App() {
+  return <NotificationList />;
+}
+
+export default App;
