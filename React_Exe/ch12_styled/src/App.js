7ef63d72752b779df2a9fc2b133a6664a3de0bb8
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-05-23 10:12:05 +0900

    styled-component 확장

--- React_Exe/ch12_styled/src/App.js
+++ React_Exe/ch12_styled/src/App.js
@@ -4,6 +4,11 @@ import ReactButton from "./ex01_non_styled/ReactButton";
 import StyledButton from "./ex02_styled/StyledButton";
 // import SimpleButton from "./ex03_styled/SimpleButton";
 import SimpleButton from "./ex04_styled/SimpleButton";
+import LargeButton from "./ex05_styled/LargeButton";
+// import PrimaryButton from "./ex06_styled/PrimaryButton";
+// import PrimaryButton from "./ex07_styled/PrimaryButton";
+import PrimaryButton from "./ex08_styled/PrimaryButton";
+import SecondButton from "./ex09_styled/SecondButton";
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
       <ReactButton>안녕하세요</ReactButton>
       <StyledButton>감사합니다</StyledButton>
       <SimpleButton>행복합니다</SimpleButton>
+      <LargeButton>존경합니다</LargeButton>
+      <PrimaryButton>사랑합니다</PrimaryButton>
+      <PrimaryButton primary>즐겁습니다</PrimaryButton>
+      <SecondButton>Happy Happy</SecondButton>
+      <SecondButton primary>Congraturation!</SecondButton>
     </>
   );
 }
