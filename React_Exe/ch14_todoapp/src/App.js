a17454fdd2500ba6dcdf482e5602ca54a35033f5
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-05-24 12:53:31 +0900

    todolist 만들기 todolistItem 생성까지

--- React_Exe/ch14_todoapp/src/App.js
+++ React_Exe/ch14_todoapp/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef, useCallback } from "react";
+import TodoTemplate from "./ex05_TodoInsert/TodoTemplate";
+import TodoInsert from "./ex05_TodoInsert/TodoInsert";
+import TodoList from "./ex05_TodoInsert/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "리액트의 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "컴포넌트 스타일링 해보기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "일정 관리 앱 만들어 보기",
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
+    </TodoTemplate>
   );
 }
 
