cce87e2a2adbf3381d6f7c12d6c7fc422d93ba94
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-05-31 12:47:09 +0900

    Rest Repoistory 서비스 구성

--- CardDateabase/src/main/java/org/mind/carddateabase/repositry/CarRepository.java
+++ CardDateabase/src/main/java/org/mind/carddateabase/repositry/CarRepository.java
@@ -2,6 +2,9 @@ package org.mind.carddateabase.repositry;
 
 import org.mind.carddateabase.domain.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+// 현재 Repository의 api 주소를 변경할 수 있다.
+//@RepositoryRestController(path = "aaa")
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
