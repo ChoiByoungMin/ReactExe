d0f7daa2e87ef6080a521249b9df87bc4ee53ea2
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-06-03 15:24:06 +0900

    cros 설정 완료

--- CardDateabase/src/main/java/org/mind/carddateabase/config/SecurityConfig.java
+++ CardDateabase/src/main/java/org/mind/carddateabase/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package org.mind.carddateabase.config;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.mind.carddateabase.component.AuthEntryPoint;
+import org.mind.carddateabase.filter.AuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +18,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 //spring security설정을 하게 되면
 //직접 서버의 주소로 법근하려고하면 무조건 login화면으로 이동한다.
@@ -39,6 +48,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
+    private final AuthenticationFilter authenticationFilter;
+    private final AuthEntryPoint authEntryPoint;
 
     // 사용자인증을 위한 userDetailsService 설정/패스워드 암호화 알고리즘 설정
     // 암호를 DB에 저장하기 전에 BCrypt 암호화 처리
@@ -59,6 +70,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // csrf보안은 세션을 활용하는데 Rest서버는 세션을 사용하지 않으므로 disable
         http.csrf().disable()
+                // CROS는 설정을 사용한다.
+                .cors().and()
                 .sessionManagement()
                 // Rest 서버는 세션 상태를 유지하지 않으므로 STATELESS
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
@@ -66,7 +79,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // /login엔드포인트에 대한 POST요청은 접근을 허용함.
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 // 다른 요청은 인증 과정을 거쳐야 접근할 수 있다.
-                .anyRequest().authenticated();
+                .anyRequest().authenticated().and()
+                // 잘못된 인증정보 요청 시, 오류 응답 처리를 authEntryPoint가 한다.
+                .exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint).and()
+                // /login을 제외한 나머지 모든 요청은 필터를 통과해야 정상 응답을 받을 수 있다.
+                .addFilterBefore(authenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+    // CORS 자세한 설정 부분
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        //config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www.bitcamp.co.kr"));
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(false);
+        config.applyPermitDefaultValues();
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
